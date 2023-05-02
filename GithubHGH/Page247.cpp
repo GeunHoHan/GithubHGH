85b646a16a2024c6f410b7b519352fe055fa5b1a
GeunHo Han <apple10715@cu.ac.kr>
2023-05-02 11:44:57 +0900

    미리 예습

---
+++ GithubHGH/Page247.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "stackS247.h"
+
+int top = -1;
+
+int isStackEmpty(void) {
+	if (top == -1) return 1;
+	else return 0;
+}
+
+int isStackFull(void) {
+
+}
+
+void push(element item) {
+
+}
+
+element pop(void) {
+
+}
+
+element peek(void) {
+
+}
+void printStack(void) {
+
+}
